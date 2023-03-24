54cf552fc260e6a9921cdac4f7fbd4c38b71cf66
Pabl088 <pnaf88@gmail.com>
2023-03-24 16:46:37 -0300

    inicio de backend con nestjs

--- api/src/config/mongodb/db.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import config from '../ConfigEntorno/config';
-
-mongoose.connect(config.Db)
-    .then(()=> console.log('database connect'))
-    .catch((error)=> console.log('database error', error))
