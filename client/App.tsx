d8b1b5a8bb76671c79ae5b4124e3a85b247a430b
TomasTinto1234 <tititinto@gmail.com>
2023-03-24 12:49:34 -0300

    client

---
+++ client/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import estilos from './App.styles'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+
+
+
+
+const styles = StyleSheet.create(estilos);
