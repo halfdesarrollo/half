d212bdc00faa8eecb027024e783130f1fee127d5
padermo <fabioaeh96@gmail.com>
2023-03-27 17:24:33 -0500

    componente buscador terminado al 70%

--- client/src/screens/MainMenuScreen.jsx
+++ client/src/screens/MainMenuScreen.jsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native"
 
+// components
+import Search from "../components/Search/Search";
+
 export const MainMenuScreen = () => {
   return (
     <View>
       <Text>MainMenuScree</Text>
+      <Search/>
     </View>
   );
 };
