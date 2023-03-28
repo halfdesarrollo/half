e26addab25389cc4b3dcae4f838ae9c7582ffb2a
Lucas Camacho <Lucascserrate@gmail.com>
2023-03-28 16:33:47 -0400

    Fonts poppins and roboto

--- client/App.jsx
+++ client/App.jsx
@@ -9,9 +9,34 @@ import store from './src/redux'
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
+//Fonts
+import {
+  useFonts as usePoppins,
+  Poppins_400Regular,
+} from '@expo-google-fonts/poppins';
+
+import {
+  useFonts as useRoboto,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto';
+
+
 const persistor = persistStore(store)
 
 export default function App() {
+
+  //<!Fonts
+  const [poppinsLoaded] = usePoppins({
+    Poppins_400Regular,
+  });
+  const [robotoLoaded] = useRoboto({
+    Roboto_400Regular,
+  });
+
+  if (!poppinsLoaded || !robotoLoaded) return null
+
+  //>
+
   return (
     <>
       <StatusBar style='auto' />
