7336c8f3518be604f92528c4927bd4228357966d
bastian <bastiangarcesga@gmail.com>
2023-03-27 21:33:08 -0300

    ADD: create login screen

--- client/Main.jsx
+++ client/Main.jsx
@@ -1,17 +1,19 @@
-import { View, Text } from 'react-native'
-import { Routes, Route } from 'react-router-native'
+import { View, Text } from "react-native";
+import { Routes, Route } from "react-router-native";
 
 //Screens
-import { LandingScreen } from './src/screens/LandingScreen'
-import { MainMenuScreen } from './src/screens/MainMenuScreen'
+import { LandingScreen } from "./src/screens/LandingScreen";
+import { LogRegScreen } from "./src/screens/LogRegScreen";
+import { MainMenuScreen } from "./src/screens/MainMenuScreen";
 
 export default function Main() {
   return (
     <View>
       <Routes>
-        <Route path='/landing' element={<LandingScreen />} />
-        <Route path='/' element={<MainMenuScreen />} />
+        <Route path="/login" element={<LogRegScreen />} />
+        <Route path="/landing" element={<LandingScreen />} />
+        <Route path="/" element={<MainMenuScreen />} />
       </Routes>
     </View>
-  )
+  );
 }
--- client/src/screens/LogRegScreen.jsx
+++ client/src/screens/LogRegScreen.jsx
@@ -1,9 +1,124 @@
-import { View, Text } from "react-native"
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  email: yup.string().email("Email is not valid").required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
 
 export const LogRegScreen = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <View>
-      <Text>LogRegScreen</Text>
+    <View style={styles.container_logReg}>
+      <View style={styles.container_logo}>
+        <Text>Logo Half</Text>
+      </View>
+
+      <View style={styles.container_inputs}>
+        <Text style={styles.title}>Iniciar Sesion</Text>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { value, onBlur, onChange } }) => (
+            <TextInput
+              style={styles.input}
+              placeholder="Correo Electronico"
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+            />
+          )}
+          defaultValue=""
+        />
+        {errors.email && <Text>{errors.email.message}</Text>}
+
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { value, onBlur, onChange } }) => (
+            <TextInput
+              style={styles.input}
+              placeholder="Contraseña"
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+            />
+          )}
+          defaultValue=""
+        />
+        {errors.password && <Text>{errors.password.message}</Text>}
+
+        <Button
+          style={styles.button}
+          title="Iniciar"
+          onPress={handleSubmit(onSubmit)}
+        />
+        <Text>Aun no tienes cuenta? Registrate aca</Text>
+      </View>
+      <View>
+        <Text>O ingresa con</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container_logReg: {
+    display: "flex",
+    backgroundColor: "white",
+    alignItems: "center",
+    width: Dimensions.get("window").width,
+    height: "100%",
+    flex: 1,
+    borderColor: "thistle",
+    paddingHorizontal: 30,
+  },
+  button: {
+    backgroundColor: "#87BE56",
+    width: Dimensions.get("window").width,
+  },
+  container_logo: {
+    backgroundColor: "red",
+    width: "100%",
+    height: "50%",
+    textAlign: "center",
+  },
+  title: {
+    textAlign: "center",
+    justifyContent: "center",
+    color: "blue",
+  },
+  container_inputs: {
+    paddingHorizontal: 20,
+    width: Dimensions.get("window").width,
+    paddingVertical: 10,
+  },
+  input: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 7,
+  },
+});
