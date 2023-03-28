37246489506235adb7c4a3d49240ead6ae358d71
TomasTinto1234 <tititinto@gmail.com>
2023-03-28 09:53:10 -0300

    Categorias

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import categories from "./categorias";
+import CategoryItem from "./CategoryItem";
+
+const Categories = () => {
+  return (
+    <View style={styles.conteiner}>
+      <Text style={styles.title}>Categorias</Text>
+      <FlatList
+      style={styles.flatList}
+        data={categories}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        renderItem={(item, i) => {
+          return <CategoryItem item={item} i={i} />;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  conteiner: {
+    fontFamily: "Poppins",
+  },
+
+  title: {
+    color: "#4B5563",
+    fontFamily: "Poppins",
+    width: 94,
+    height: 13,
+    left: 24,
+    fontSize: 20,
+    fontWeight:"bold",
+  },
+
+});
+
+export default Categories;
---
+++ client/src/components/CategoryItem.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import image from "../../assets/favicon.png";
+
+const CategoryItem = ({ item, i }) => {
+  console.log(item);
+  console.log(item.index);
+
+  return (
+    <View key={item.id} style={styles.conteiners}>
+      <View style={styles.view}>
+        <Image style={styles.categoryImage} source={{ uri: item.item.imagen }} />
+      </View>
+      <View style={styles.nombres}>
+        <Text
+          style={
+            item.index % 2 === 0
+              ? styles.categorySecondaryColor
+              : styles.categoryPrymaryColor
+          }
+        >
+          {item.item.nombre}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  conteiners: {
+    flexDirection: "row",
+    justifyContent:"center",
+    fontFamily: "Poppins",
+    color: "#87BE56",
+    marginLeft: 25,
+    marginTop:30,
+
+  },
+  nombres:{
+    padding: 10,
+    top: 10,
+    marginTop: 55,
+    textAlign: "center",
+  },
+
+  categoryImage: {
+    width: 58,
+    height: 58,
+    borderRadius: 38,
+  },
+
+  categorySecondaryColor: {
+    color: "#87BE56",
+  },
+  categoryPrymaryColor: {
+    color: "#FFAD3F",
+  },
+});
+
+export default CategoryItem;
