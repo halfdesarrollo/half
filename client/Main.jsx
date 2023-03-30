eed146c74c06381749ae54cfb2fe0db9158dd31e
mativegal <matiasvegalera@gmail.com>
2023-03-30 14:14:13 -0300

    Add pay method screen

--- client/Main.jsx
+++ client/Main.jsx
@@ -1,19 +1,21 @@
-import { View } from "react-native";
-import { Routes, Route } from "react-router-native";
-import { LoginScreen } from "./src/screens/LoginScreen";
-import { WelcomeScreen } from "./src/screens/WelcomeScreen";
-import { MainMenuScreen } from "./src/screens/MainMenuScreen";
-import { HowDoesItWorksScreen } from "./src/screens/HowDoesItWorksScreen";
+import { View } from 'react-native'
+import { Routes, Route } from 'react-router-native'
+import { LoginScreen } from './src/screens/LoginScreen'
+import { WelcomeScreen } from './src/screens/WelcomeScreen'
+import { MainMenuScreen } from './src/screens/MainMenuScreen'
+import { HowDoesItWorksScreen } from './src/screens/HowDoesItWorksScreen'
+import { AddPayMethodScreen } from './src/screens/HowDoesItWorksScreen'
 
 export default function Main() {
   return (
     <View>
       <Routes>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/welcome" element={<WelcomeScreen />} />
-        <Route path="/howdoesitworks" element={<HowDoesItWorksScreen />} />
-        <Route path="/" element={<MainMenuScreen />} />
+        <Route path='/login' element={<LoginScreen />} />
+        <Route path='/welcome' element={<WelcomeScreen />} />
+        <Route path='/howdoesitworks' element={<HowDoesItWorksScreen />} />
+        <Route path='/' element={<MainMenuScreen />} />
+        <Route path='/addpaymethod' element={<AddPayMethodScreen />} />
       </Routes>
     </View>
-  );
+  )
 }
