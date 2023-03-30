6a89f6cf20fcdc4fe384b2b99fd81439cabcad48
mativegal <matiasvegalera@gmail.com>
2023-03-29 23:23:16 -0300

    Add waiter button and modals

--- client/src/screens/MainMenuScreen.jsx
+++ client/src/screens/MainMenuScreen.jsx
@@ -1,37 +1,34 @@
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
+import { View, Text } from 'react-native'
+import { StyleSheet } from 'react-native'
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
-import Logo from "../../assets/halflogo.svg";
-import Navbar from "../components/Navbar";
+} from 'react-native-responsive-screen'
+import Navbar from '../components/Navbar'
 // components
 
-import SpecialHalf from "../components/SpecialHalf"
-import Search from "../components/Search/Search";
-
+import SpecialHalf from '../components/SpecialHalf'
+import Search from '../components/Search'
 
 export const MainMenuScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>MainMenuScree</Text>
       <Search />
       <Navbar />
-      <SpecialHalf/>
+      <SpecialHalf />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    width: wp("100.00%"),
-    height: hp("100.00%"),
-    alignItems: "center",
-    backgroundColor: "white",
+    width: wp('100.00%'),
+    height: hp('100.00%'),
+    alignItems: 'center',
+    backgroundColor: 'white',
   },
   logo: {
-    width: wp("50.00%"),
-    height: hp("30.00%"),
+    width: wp('50.00%'),
+    height: hp('30.00%'),
   },
-});
+})
