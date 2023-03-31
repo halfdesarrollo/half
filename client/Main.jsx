80430bc1f410a0c759018173a1f8625cac24579e
bastian <bastiangarcesga@gmail.com>
2023-03-31 19:23:21 -0300

    pr

--- client/Main.jsx
+++ client/Main.jsx
@@ -6,6 +6,7 @@ import { MainMenuScreen } from "./src/screens/MainMenuScreen";
 import { HowDoesItWorksScreen } from "./src/screens/HowDoesItWorksScreen";
 import { AddPayMethodScreen } from "./src/screens/AddPayMethodScreen";
 import { QrScreen } from "./src/screens/QrScreen";
+import { MenuCardScreen } from "./src/screens/MenuCardScreen";
 
 export default function Main() {
   return (
@@ -15,7 +16,7 @@ export default function Main() {
         <Route path="/qrscreen" element={<QrScreen />} />
         <Route path="/welcome" element={<WelcomeScreen />} />
         <Route path="/howdoesitworks" element={<HowDoesItWorksScreen />} />
-        <Route path="/" element={<LoginScreen />} />
+        <Route path="/" element={<MainMenuScreen />} />
         <Route path="/addpaymethod" element={<AddPayMethodScreen />} />
       </Routes>
     </View>
--- client/metro.config.js
+++ client/metro.config.js
@@ -1,20 +1,21 @@
-const { getDefaultConfig } = require('@expo/metro-config')
-
+const { getDefaultConfig } = require("@expo/metro-config");
+const { withMetroConfig } = require("@expo/metro-config");
 module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname)
+  const config = await getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config
+  const { transformer, resolver } = config;
 
   config.transformer = {
     ...transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  }
-  config.resolver.assetExts.push('cjs')
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+
+  config.resolver.assetExts.push("cjs");
   config.resolver = {
     ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
-  }
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
 
-  return config
-})()
+  return config;
+})();
