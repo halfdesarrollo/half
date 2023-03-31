d195cc6163d3fa3252140f8ed2efb43d48602df4
mativegal <matiasvegalera@gmail.com>
2023-03-31 18:34:14 -0300

    Add screens

---
+++ client/src/screens/ViewOrderScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ViewOrderScreen = () => {
+  return (
+    <View>
+      <Text>ViewOrderScreen</Text>
+    </View>
+  )
+}
+
+export default ViewOrderScreen
+
+const styles = StyleSheet.create({})
