448adb9e62bcdfdc726c84627e87e46ffe9ff936
Lucas Camacho <Lucascserrate@gmail.com>
2023-03-31 23:08:57 -0400

    categories mock

--- client/src/mocks/categorias.js
+++ client/src/mocks/categorias.js
@@ -1,27 +1,26 @@
 export default [
-    {
-      id: 1,
-      nombre: "bares",
-      imagen: "https://gastronomiaycia.republica.com/wp-content/uploads/2021/12/lista_bar_2021_paradise-680x447.jpg",
-    },
-    {
-      id: 3,
-      nombre: "cafes",
-      imagen: "https://st.depositphotos.com/1020804/1263/i/600/depositphotos_12639245-stock-photo-coffee-cup-and-saucer-on.jpg",
-    },
-    {
-      id: 4,
-      nombre: "fastfood",
-      imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSot-RhgyL6H_yUmPGjmQxp5E-D2rLDg4R1y83-pQI0PA&s",
-    },
-    {
-      id: 2,
-      nombre: "restaurante",
-      imagen: "https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/comer/top-de-gastronomia/los-restaurantes-mas-antiguos-de-espana/gr-cms-media-featured_images-none-759f762b-7965-41cd-a979-da49509e205d-1.jpg",
-    },
-    
-    
-  ];
-  
+  {
+    id: 1,
+    name: "Bares",
+    img: "https://gastronomiaycia.republica.com/wp-content/uploads/2021/12/lista_bar_2021_paradise-680x447.jpg",
+  },
+  {
+    id: 3,
+    name: "Cafes",
+    img: "https://s1.eestatic.com/2017/02/13/cocinillas/cocinillas_193495389_116293002_1024x576.jpg",
+  },
+  {
+    id: 4,
+    name: "Fastfood",
+    img: "https://images.unsplash.com/photo-1624855600799-ac8e8bddd1da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzdGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
+  },
+  {
+    id: 2,
+    name: "Resta...",
+    img: "https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/comer/top-de-gastronomia/los-restaurantes-mas-antiguos-de-espana/gr-cms-media-featured_images-none-759f762b-7965-41cd-a979-da49509e205d-1.jpg",
+  },
 
-  
+
+];
+
+
