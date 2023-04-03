2279d3964ec8e93a4854d44bfa6bcc5a7f4fe11d
mativegal <matiasvegalera@gmail.com>
2023-04-03 17:22:18 -0300

    Add Redux States

--- client/src/redux/slices/user/userSlice.js
+++ client/src/redux/slices/user/userSlice.js
@@ -1,49 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
-import jwt_decode from "jwt-decode";
-import axios from "axios";
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   user: {},
   token: null,
-};
+  contacts: [],
+}
 
 export const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.user;
+      state.user = action.payload.user
+      state.token = action.payload.user
     },
 
     logoutUser: (state, action) => {
-      state.user = {};
+      state.user = {}
     },
   },
-});
+})
 
-export const login = (email, password) => async (dispatch) => {
-  try {
-    const response = await axios.post(URL_LOGIN, {
-      email: email,
-      password: password,
-    });
-    const userLogged = jwt_decode(response.data.access_token);
-    dispatch(
-      loginUser({ user: { userLogged }, token: response.data.access_token })
-    );
-  } catch (error) {
-    if (error.response) {
-      console.log("Error de respuesta:", error.response.data);
-      console.log("Código de estado:", error.response.status);
-    } else if (error.request) {
-      console.log("No se recibió respuesta:", error.request);
-    } else {
-      console.log("Error:", error.message);
-    }
-    console.log("Configuración del error:", error.config);
-  }
-};
-
-export const { loginUser, logoutUser } = userSlice.actions;
-export default userSlice.reducer;
+export const { loginUser, logoutUser } = userSlice.actions
+export default userSlice.reducer
