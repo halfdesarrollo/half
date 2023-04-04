cbb4110f30d7fd02ab6384765634d9710e3afa8b
braianpx <braivaldez16@gmail.com>
2023-04-03 23:46:19 -0300

    create component PaymentProof

--- client/src/components/InputReutil.jsx
+++ client/src/components/InputReutil.jsx
@@ -1,45 +1,83 @@
-import React, { useState } from 'react';
-import { View, Switch, TextInput } from 'react-native';
+import { 
+  View,
+  TextInput, 
+  Text, 
+  StyleSheet 
+} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import { colors, fonts } from '../utils/theme';
 
-const InputReutil = ({state, setState, errors, setErrors}) => {
-  const [isEnabled, setIsEnabled] = useState(false);
- 
- 
-  const toggleSwitch = () => {
-    setIsEnabled(previousState => !previousState);
-  };
-  
-  const handleInputChange = () => {
-
+const InputReutil = ({
+  state, 
+  setState, 
+  nameInput, 
+  errors, 
+  setErrors, 
+  textLabel,
+  type,
+  secure
+}) => {
+  const handleInputChange = (value) => {
+    setErrors && setErrors({
+      ...errors,
+      [state.nameInput] : value,
+    })
+    setState({
+      ...state,
+      [state.nameInput] : value,
+    })
   }
-
   return(
-    <View>
-  {/* <Switch
-    trackColor={{ false: "#767577", true: "#81b0ff" }}
-    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-    ios_backgroundColor="#3e3e3e"
-    onValueChange={toggleSwitch}
-    value={isEnabled}
-  /> */}
-</View>
-
-    // <View >
-    //   <Text>{textLabel}</Text>
-    //   <TextInput
-    //     value={state}
-    //     onChange={handleInputChange}
-    //   />
-    // </View >
+    <View style={styles.container} >
+      <Text style={styles.label}>{textLabel || 'Label'}</Text>
+      <TextInput
+        name={nameInput}
+        style={styles.input}
+        value={state.InputReutil}
+        keyboardType={type}
+        secureTextEntry={secure||false}
+        onChangeText={handleInputChange}
+      />
+      {errors?.[nameInput] && 
+        <Text style={[ styles.label, styles.errorText ]}>
+          { errors[nameInput] }
+        </Text>
+      }
+    </View >
   )
 }
 
-// const styles = StyleSheet.create({
-//   label:{
-//   },
-//   input:{
-    
-//   }
-// });
+const styles = StyleSheet.create({
+  container:{
+    justifycontent:'center',
+    alignItems:'center',
+    marginTop:20,
+  },
+  input:{
+    width: wp(89),
+    height: 48,
+    borderColor:'#BEC5D1',
+    borderWidth:1,
+    borderRadius:5,
+    padding:10,
+    fontFamily: fonts.poppins.medium,
+    fontSize: 14,
+    color:colors.secundary2,
+  },
+  label:{
+    width:wp(84),
+    height: 36,
+    color:colors.secundary2,
+    fontFamily: fonts.poppins.medium,
+    fontStyle: 'normal',
+    fontSize: 14,
+    lineHeight: 36,
+  },
+  errorText:{
+    color:'#EB5757'
+  }
+})
 
 export default InputReutil;
