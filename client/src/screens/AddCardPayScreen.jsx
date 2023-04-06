eb606a16ab38f49e6f91278f3073e19b7b8ee79d
padermo <fabioaeh96@gmail.com>
2023-04-06 16:08:32 -0500

    front demo 72

---
+++ client/src/screens/AddCardPayScreen.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TitlePage from '../components/TitlePage'
+import NewCardPay from '../components/NewCardPay'
+
+const AddCardPayScreen = () => {
+  return (
+    <View>
+      <TitlePage text={'Agregar tarjeta'} route={'/addpaymethod'}/>
+      <NewCardPay/>
+    </View>
+  )
+}
+
+export default AddCardPayScreen
+
+const styles = StyleSheet.create({})
