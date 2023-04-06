6bc4e907d0f41d4114ca7358b33a995a29d17602
TomasTinto1234 <tititinto@gmail.com>
2023-04-06 14:58:10 -0300

    cambios-modal

--- client/src/components/ModalPaymentConfirmed.jsx
+++ client/src/components/ModalPaymentConfirmed.jsx
@@ -1,62 +1,67 @@
-import { 
-  StyleSheet,
-  View,
-  Text
-} from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { fonts, colors } from "../utils/theme";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+} from "react-native-responsive-screen";
 import Confeti from "../../assets/confeti";
 import ButtonReutil from "./ButtonReutil";
+import { useNavigate } from "react-router-native";
 
-const ModalPaymentConfirmed = ({paymentCode}) => {
-  return(
-      <View style={styles.container}>
-        <View style={styles.containerDatas}>
-          <Confeti style={styles.svg}/>
-          <Text style={styles.title}>Pago Confirmado</Text>
-          <Text style={styles.subTitle}>Código de pago: {paymentCode || '#9876543'}</Text>
-          <ButtonReutil 
-            colorBtn={styles.button} 
-            styleText={styles.textButton}
-            action={()=>null}
-            text={"Revisar Comprobante"}/>
-        </View>
-        <View style={styles.containerBackground}></View>
+const ModalPaymentConfirmed = ({ paymentCode }) => {
+  const navigation = useNavigate();
+  const handleRediret = () => {
+    navigation("/menucard/1")
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.containerDatas}>
+        <Confeti style={styles.svg} />
+        <Text style={styles.title}>Pago Confirmado</Text>
+        <Text style={styles.subTitle}>
+          Código de pago: {paymentCode || "#9876543"}
+        </Text>
+        <ButtonReutil
+          colorBtn={styles.button}
+          styleText={styles.textButton}
+          action={handleRediret}
+          text={"Revisar Comprobante"}
+        />
       </View>
-   )
-}
+      <View style={styles.containerBackground}></View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container:{
-    width: wp('100.00%'),
-    height: hp('100.00%'),
-    alignItems:'center',
-    justifyContent:'center',
-    position:'absolute',
+  container: {
+    width: wp("100.00%"),
+    height: hp("100.00%"),
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
   },
-  containerBackground:{
-    width: wp('100.00%'),
-    height: hp('100.00%'),
-    position:'absolute',
-    backgroundColor:'#000000',
-    opacity:0.5,
+  containerBackground: {
+    width: wp("100.00%"),
+    height: hp("100.00%"),
+    position: "absolute",
+    backgroundColor: "#000000",
+    opacity: 0.5,
   },
-  containerDatas:{
-    width:wp(90),
+  containerDatas: {
+    width: wp(90),
     height: hp(55),
-    backgroundColor:'#FFFFFF',
-    borderRadius:10,
-    justifyContent:'center',
-    alignItems:'center',
-    position:'absolute',
-    zIndex:2,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    zIndex: 2,
   },
-  svg:{
-    width:wp(50),
-    height:hp(20),
+  svg: {
+    width: wp(50),
+    height: hp(20),
   },
   button: {
     flexDirection: "row",
@@ -72,31 +77,31 @@ const styles = StyleSheet.create({
     shadowColor: "#000000",
     shadowOffset: { width: 1, height: 3 },
     shadowOpacity: 0.2,
-    paddingVertical:15,
-    paddingHorizontal:20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
   },
   textButton: {
-      color: "#ffff",
-      fontStyle: "normal",
-      fontSize: 14,
-      lineHeight: 24,
-      fontFamily: fonts.poppins.medium,
-      textAlign: "center",
-      letterSpacing: -0.01,
+    color: "#ffff",
+    fontStyle: "normal",
+    fontSize: 14,
+    lineHeight: 24,
+    fontFamily: fonts.poppins.medium,
+    textAlign: "center",
+    letterSpacing: -0.01,
   },
-  title:{
-    marginTop:16,
-    fontSize:24,
-    fontFamily:fonts.poppins.regular,
-    color:colors.primaryGreen
+  title: {
+    marginTop: 16,
+    fontSize: 24,
+    fontFamily: fonts.poppins.regular,
+    color: colors.primaryGreen,
   },
-  subTitle:{
-    paddingTop:0,
-    marginTop:0,
-    color:colors.secundary8,
-    fontSize:12,
-    fontFamily: fonts.poppins.regular
-  }
-})
+  subTitle: {
+    paddingTop: 0,
+    marginTop: 0,
+    color: colors.secundary8,
+    fontSize: 12,
+    fontFamily: fonts.poppins.regular,
+  },
+});
 
 export default ModalPaymentConfirmed;
