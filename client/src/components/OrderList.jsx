cc29959f4c20a3df56dcb8aa203c16a963fa2da5
Lucas Camacho <Lucascserrate@gmail.com>
2023-04-11 17:55:53 -0400

    limpiando el projecto

--- client/src/components/OrderList.jsx
+++ client/src/components/OrderList.jsx
@@ -21,7 +21,7 @@ const OrderList = ({ screen, dish }) => {
           <View style={styles.spaceText}>
             <View>
               <Text style={styles.text}>{dish.name}</Text>
-              <Text style={styles.text}>S/.22.90</Text>
+              <Text style={styles.text}>{`S/.${dish.price.toFixed(2)}`}</Text>
             </View>
             <View style={styles.quantIcon}>
               <Text style={styles.text}>{dish.quantity}</Text>
