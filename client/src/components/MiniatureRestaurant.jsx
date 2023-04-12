4fb78ce71639e98f4a904e160dd3c6cf642380dc
TomasTinto1234 <tititinto@gmail.com>
2023-04-11 21:32:27 -0300

    miniatureRestaurant

--- client/src/components/MiniatureRestaurant.jsx
+++ client/src/components/MiniatureRestaurant.jsx
@@ -4,16 +4,17 @@ import ImageRestaurant from "../../assets/ImageRestaurant.svg";
 import Discount from "../../assets/Discount.svg";
 import HeartLike from "../../assets/HeartLike.svg";
 import Stars from "../../assets/stars.svg";
+import { fonts } from "../utils/theme";
 
 const MiniatureRestaurant = () => {
   return (
     <View style={styles.container}>
       <HeartLike style={styles.heart} />
       <ImageRestaurant style={styles.imagerestaurant} />
-      <View style={styles.container2}>
+      <View style={styles.text}>
         <Discount style={styles.discount} />
         <View style={styles.nameCheck}>
-          <Text style={styles.nameRestaurant}></Text>
+          <Text></Text>
         </View>
         <Stars />
       </View>
@@ -25,8 +26,7 @@ const styles = StyleSheet.create({
   container: {
     width: 318,
     height: 160,
-    left: 5,
-    top: 5,
+    margin: 5,
     borderRadius: 15,
     backgroundColor: "#FFFFFF",
     borderRadius: 15,
@@ -39,21 +39,11 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
   },
-  container2: {
+  text: {
     top: 50,
   },
   heart: {
     position: "absolute",
-  },
-  nameRestaurant: {
-    width: 112,
-    height: 19,
-    left: 44,
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: 700,
-    fontSize: 20,
-    lineHeight: 23,
   },
   imagerestaurant: {
     position: "absolute",
