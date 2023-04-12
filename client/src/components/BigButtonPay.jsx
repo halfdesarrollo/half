ad1e805ac4a2d124ceb166a459e7e179c58f7876
mativegal <matiasvegalera@gmail.com>
2023-04-12 16:07:40 -0300

    change

--- client/src/components/BigButtonPay.jsx
+++ client/src/components/BigButtonPay.jsx
@@ -1,20 +1,16 @@
 import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import { useNavigate } from 'react-router-native'
 import { colors, fonts } from '../utils/theme'
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen'
-import { useDispatch, useSelector } from 'react-redux'
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { useDispatch } from 'react-redux'
 import { addAllOrder } from '../redux/slices/order/orderSlice'
 
 const BigButtonPay = ({ text, route, order = false }) => {
   const dispatch = useDispatch()
-  const { preOrderPrice } = useSelector((state) => state.orderState)
   const navigate = useNavigate()
   const handlePress = () => {
     if (order) {
-      dispatch(addAllOrder(preOrderPrice))
+      dispatch(addAllOrder())
     }
 
     navigate(route)
@@ -37,8 +33,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
-    fontSize: 17,
-    fontWeight: 'bold',
+    fontSize: 16,
     textAlign: 'center',
     fontFamily: fonts.poppins.bold,
   },
